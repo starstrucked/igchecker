f2946c00cc5e26454ee656b8f6c16645be226c18
starstrucken <85191157+starstrucken@users.noreply.github.com>
2022-09-11 14:45:16 -0700

    Add files via upload

---
+++ IG-Checker.py
@@ -0,0 +1,41 @@
+############################################
+#  Instagram Username Availability Checker #
+#  Github.com/starstrucken                 #
+############################################
+import requests, os
+from pystyle import Colors
+from threading import Thread
+import concurrent.futures
+from time import sleep
+os.system('cls||clear')
+print(f""" 
+{Colors.blue}//{Colors.white} IG Username Checker 
+""")
+print("")
+username_file = input(f"{Colors.blue}\{Colors.reset}?{Colors.blue}\{Colors.reset} Usernames File: ")
+availablecount = 0
+takencount = 0
+errorcount = 0
+def title():  
+    while True:
+        os.system(f"title Taken:{[takencount]}   Available:{[availablecount]}  Errors:{[errorcount]}")
+def check():
+ try:
+  while True:
+   openusers = open(username_file,"r").read().splitlines()
+   for username in openusers:
+    global availablecount, takencount, errorcount
+    re = requests.post(f'https://instagram.com/{username}')
+    if re.text.find(',"target_id":"') >= 0:
+     takencount +=1
+     print(f"/{Colors.red}X{Colors.reset}/ Taken: [{username}]")
+    elif re.text == '':
+     errorcount +=1
+    else:
+     availablecount +=1
+     print(f"/{Colors.green}+{Colors.reset}/ Available: [{username}]")
+ except:
+  errorcount +=1
+  pass
+Thread(target=(title)).start()
+Thread(target=(check)).start()
